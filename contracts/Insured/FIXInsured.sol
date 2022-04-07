6c71e1d14c31d2bb8c09ceae2a3433eca608123d
Qiao Jiang <qiaojiang@qiaos-mbp.myfiosgateway.com>
2022-04-07 00:19:28 -0400

    04/07/2022

--- contracts/Insured/FIXInsured.sol
+++ contracts/Insured/FIXInsured.sol
@@ -5,7 +5,14 @@ pragma solidity ^0.8.0;
 
 Variables to include:
 
-enum
+enum POLICY_STATE has 6 states:
+
+OPEN_UNVERIFIED: An insured deployed a smart contract to request an insurance policy but the eligibility is not verified
+OPEN_VERIFIED: The eligibility is verified. Open to accept insurers.
+AUCTION: The number of insurers meet a certain criterion. Ready to pick insurers.
+ACTIVE_POLICY: Policy is effective
+ACCIDENT_VERIFIED: An accident occurs and is verified by Accident Verifier.
+CLOSED: Policy is closed
 
 Functions to include:
 
@@ -14,4 +21,13 @@ Functions to include:
 
 */
 
-contract FIXInsurer {}
+contract FIXInsurer {
+    enum POLICY_STATE {
+        OPEN_UNVERIFIED,
+        OPEN_VERIFIED,
+        AUCTION,
+        ACTIVE_POLICY,
+        ACCIDENT_VERIFIED,
+        CLOSED
+    }
+}
